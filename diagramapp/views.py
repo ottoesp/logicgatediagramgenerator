f7fe40d891092ed8d06dd469da1a507d7d23d7a3
ottoesp <otto.esperson@gmail.com>
2025-09-18 15:39:24 +1000

    initialised urls and db

---
+++ diagramapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello World! We at the index ovah here")
+
+# Create your views here.
--- ldg/urls.py
+++ ldg/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path("diagramapp/", include("diagramapp.urls")), # Include means redirect if suffix matches
     path('admin/', admin.site.urls),
 ]
