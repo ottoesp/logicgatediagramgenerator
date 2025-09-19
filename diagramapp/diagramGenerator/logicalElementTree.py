7ca4b82c79e9bed4cb0d82cb123baed19e035ff8
ottoesp <otto.esperson@gmail.com>
2025-09-19 10:45:38 +1000

    Converted over from tree to dag

--- diagramapp/diagramGenerator/logicalElementTree.py
+++ diagramapp/diagramGenerator/logicalElementTree.py
@@ -1,27 +1,12 @@
 from collections import defaultdict
 from typing import Tuple
 
-from connectives import LogicalElement
-
-class LogElNode:
-    def __init__(self):
-        self.data = LogicalElement()
-        self.left = None
-        self.right = None
-
-
-    def is_empty(self):
-        return self.data.is_empty()
-
-    def set_data(self, data):
-        self.data = data
-
 class DiagramNode:
     nextId = 0
     variables = []
     def __init__(self, name):
         self.name = name
-        self.id = DiagramNode.nextId
+        self.id = str(DiagramNode.nextId)
         DiagramNode.nextId += 1
 
     def get_id(self):
@@ -32,21 +17,46 @@ class VariableNode(DiagramNode):
     def __init__(self, name):
         super().__init__(name)
         self.name = name
-
-    def get_id(self):
-        return self.name
-
+        self.id = name
 
 class DiagramDag:
     def __init__(self):
         self.nodes: set[DiagramNode] = set()
-        self.edges: set[Tuple[DiagramNode, DiagramNode]] = set()
+        self.edges: set[Tuple[str, str]] = set()
 
         self.adj = defaultdict(list)
 
+    def get_node_ids(self):
+        return map(lambda node: node.get_id(), self.nodes)
+
     def insert_node(self, node: DiagramNode, parent_node: DiagramNode = None):
-        self.nodes.add(node)
+        if node.get_id() not in self.get_node_ids():
+            self.nodes.add(node)
         if parent_node is not None:
-            self.edges.add((parent_node, node))
-            self.adj[parent_node.get_id()].append(node)
+            self.edges.add((parent_node.get_id(), node.get_id()))
+            self.adj[parent_node.get_id()].append(node.get_id())
+        return node
+
+    def print_graph(self):
+        for node in self.nodes:
+            print(f'{node.get_id()} {node.name}')
+            for child in self.adj[node.get_id()]:
+                print(f'   -> {child}')
+            print('\n--------------')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- diagramapp/diagramGenerator/main.py
+++ diagramapp/diagramGenerator/main.py
@@ -1,20 +1,20 @@
-from utils import print_tree
-
-from logicalElementTree import LogElNode
+from logicalElementTree import DiagramNode, DiagramDag
 from parseWff import  parse_wff
 
 def generate_diagram(wff):
-    tree = LogElNode()
+    dag = DiagramDag()
+    root = DiagramNode("dummy")
+    dag.insert_node(root)
     # Need this to not be a tree but actually have variables combining
     # Then use Kahn's algorithm https://en.wikipedia.org/wiki/Topological_sorting
     # for topological sorting into Coffman-Grahams Algorithm https://en.wikipedia.org/wiki/Coffman%E2%80%93Graham_algorithm
     # for optimal levels
     # Then https://en.wikipedia.org/wiki/Layered_graph_drawing
 
-    parse_wff(wff, tree)
-    print_tree(tree)
+    parse_wff(wff, dag, root)
+    dag.print_graph()
 
-generate_diagram("not A or (A and not B) or C")
+# generate_diagram("not A or (A and not B) or C")
 # generate_diagram("A")
 #
 generate_diagram("(A or not (A and B))")
