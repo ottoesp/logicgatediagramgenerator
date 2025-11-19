4485784653aa09d8370f58faf54fc1440b6538e0
ottoesp <otto.esperson@gmail.com>
2025-11-19 13:54:21 +1100

    Enum based default charset

--- diagramapp/diagramGenerator/render/displayElement.py
+++ diagramapp/diagramGenerator/render/displayElement.py
@@ -1,8 +1,18 @@
 class DisplayElement:
-    pass
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def render(self):
+        pass
 
 class Path(DisplayElement):
     pass
 
 class Node(DisplayElement):
-    pass
+    def __init__(self, x, y):
+        super().__init__(x, y)
+
+class AndGate(Node):
+    def __init__(self, x, y):
+        super().__init__(x, y)
