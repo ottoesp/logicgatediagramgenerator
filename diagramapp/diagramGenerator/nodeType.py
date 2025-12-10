00d79091dd33ef1a709d491b6a2a08d6c9d2643e
ottoesp <otto.esperson@gmail.com>
2025-12-10 22:38:41 +1100

    Added some more dicts

--- diagramapp/diagramGenerator/nodeType.py
+++ diagramapp/diagramGenerator/nodeType.py
@@ -9,12 +9,27 @@ class NodeType(Enum):
     NONE = 6
     ROOT = 7
 
+STRING_REPRESENTATION = {
+    NodeType.AND : 'and',
+    NodeType.OR : 'or',
+    NodeType.NOT : 'not',
+}
+
+TYPE_FROM_STRING_REPRESENTATION = {v: k for k, v in STRING_REPRESENTATION.items()}
+
 NUMBER_OF_INPUTS = {
     NodeType.AND : 2,
     NodeType.OR : 2,
     NodeType.NOT : 1,
     NodeType.DUMMY : 1,
     NodeType.ROOT : 1
+}
+
+BINDING_STRENGTH = {
+    NodeType.AND : 1,
+    NodeType.OR : 1,
+    NodeType.NOT : 2,
+    NodeType.VARIABLE : 3
 }
 
 VERTICAL_SIZE = {
@@ -34,3 +49,6 @@ PREFERS_OFFSET = {
     NodeType.ROOT : False,
     NodeType.VARIABLE : False
 }
+
+DOUBLE_INPUT_GATES = {NodeType.AND, NodeType.OR}
+SINGLE_INPUT_GATES = {NodeType.NOT}
